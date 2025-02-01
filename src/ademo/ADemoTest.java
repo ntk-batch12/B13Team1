ac95bb454405bc54326f388b94ad3a68b9df68f1
issam <issam@RPC-2RVWMQ2>
2025-01-31 22:06:11 -0500

    HomeWork - Push Number# 1

--- src/ademo/ADemoTest.java
+++ src/ademo/ADemoTest.java
@@ -16,6 +16,17 @@ public class ADemoTest {
 			System.out.println(i);
 		}
 		System.out.println("GitHub is fum so far ...");
+		
+		
+		// Adding new lines 1/31/2025 - Push Number# 1
+		System.out.println("***** From Sam: Push Number 1 *****");
+		int count = 10;
+		if (count > 20) {
+			System.out.println("Count is Greater than 20 ");
+		} else {
+			System.out.println("Count is Less than 20");
+		}
+		System.out.println("****** End of Code by Sam ******");
 	}
 	
 }
