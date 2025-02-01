945d3df427ce8954198e8413cc72bab15c88b6c5
issam <issam@RPC-2RVWMQ2>
2025-01-31 21:28:33 -0500

    Two Println lines were added.

--- src/ademo/ADemoTest.java
+++ src/ademo/ADemoTest.java
@@ -4,6 +4,9 @@ public class ADemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Team 1");
+		
+		System.out.println("Hi Team 1");
+		System.out.println("This is just a test");
 	}
 	
 }
